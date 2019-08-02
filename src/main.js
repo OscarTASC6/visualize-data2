c744ac63eae829111cc53b9a74dbe99d20418724
OscarTASC6 <oscart5292@rfwagner.org>
2019-08-02 15:23:21 -0400

    Co-authored-by: giovanniASC6 <giovanni.owens@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,21 +1,53 @@
-const first = list[0]
-const main =document.querySelector("main")
-const createCard= obj => {
-const newCard= document.createElement("div")
-newCard.className="card"
+// const first = list[0]
+const main = document.querySelector("main")
 
-const face=document.createElement("img")
-face.src=obj.picture.medium
-face.alt="Portrait";
-
-
-newCard.appendChild(face);
-    
-
-
-
-
-main.appendChild(newCard)
+const createCard = (obj, indx) => {
+    const newCard = document.createElement("div")
+    newCard.className = "card"
+    newCard.id = indx
+    const face = document.createElement("img")
+    face.className = "people"
+    face.src = obj.picture.large
+    face.alt = "Portrait";
+    newCard.appendChild(face);
+    main.appendChild(newCard)
 };
 
-createCard(first);
+// createCard(first);
+
+const loopcreate = arr => {
+    for (let i = 0; i < arr.length; i++) {
+        createCard(arr[i], i);
+    }
+}
+loopcreate(list)
+
+document.getElementsByClassName("main-element")
+
+const images = document.getElementsByClassName("people");
+const imagesArray = [... images]
+
+for (let i = 0; i<imagesArray.length; i ++){
+    imagesArray[i].style.opacity = "1";
+    imagesArray[i].style.transition = ".5s ease";
+    imagesArray[i].style.backgroundColor = "red";
+    currentElement= imagesArray[i];
+    imagesArray[i].addEventListener("mouseover", diffCss)
+    imagesArray[i].addEventListener("mouseout", sameCss)
+}
+
+
+
+function sameCss(event){
+    event.target.style.opacity = "1";
+}
+
+function diffCss(){
+    event.target.style.opacity = "0";
+    event.target.style.backgroundColor= "red";
+}
+
+
+
+
+
