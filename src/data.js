5b03365ee96af975aec53a7b9c2f835b16ceb6aa
OscarTASC6 <oscart5292@rfwagner.org>
2019-08-02 11:59:39 -0400

    portrait

---
+++ src/data.js
@@ -0,0 +1,271 @@
+const data={
+    "results": [
+        {
+            "gender": "male",
+            "name": {
+                "title": "monsieur",
+                "first": "federico",
+                "last": "lemoine"
+    },
+            "location": {
+                "street": "690 rue dumenge",
+                "city": "tartegnin",
+                "state": "obwalden",
+                "postcode": 8274,
+                "coordinates": {
+                    "latitude": "-58.2018",
+                    "longitude": "-51.6820"
+    },
+                "timezone": {
+                    "offset": "-2:00",
+                    "description": "Mid-Atlantic"
+    }
+            },
+            "email": "federico.lemoine@example.com",
+            "login": {
+                "uuid": "4861dd8f-06b6-4979-a209-aeeacf93e18e",
+                "username": "angrymeercat679",
+                "password": "birthday1",
+                "salt": "czTAJBLk",
+                "md5": "989f5a1bc450954fa04bfa70382823c4",
+                "sha1": "21cf349505f79c21a3ff4c3ee9f64884f5f1fa9f",
+                "sha256": "d99124571dbe1b80d76315fefabc3b93da61e0da7be0e9896b5aea7e74c1bf40"
+    },
+            "dob": {
+                "date": "1990-11-17T13:17:42Z",
+                "age": 28
+    },
+            "registered": {
+                "date": "2017-02-12T06:51:19Z",
+                "age": 2
+    },
+            "phone": "(962)-905-9094",
+            "cell": "(257)-509-1133",
+            "id": {
+                "name": "AVS",
+                "value": "756.1535.8622.46"
+    },
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/men/66.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/66.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/66.jpg"
+    },
+            "nat": "CH"
+    },
+        {
+            "gender": "male",
+            "name": {
+                "title": "mr",
+                "first": "gustav",
+                "last": "kristensen"
+    },
+            "location": {
+                "street": "8325 havbakken",
+                "city": "sørvad",
+                "state": "midtjylland",
+                "postcode": 34741,
+                "coordinates": {
+                    "latitude": "-74.5264",
+                    "longitude": "145.9732"
+    },
+                "timezone": {
+                    "offset": "+3:00",
+                    "description": "Baghdad, Riyadh, Moscow, St. Petersburg"
+    }
+            },
+            "email": "gustav.kristensen@example.com",
+            "login": {
+                "uuid": "748b80f1-2551-4868-9b44-7a52558d4d74",
+                "username": "browndog369",
+                "password": "pumpkins",
+                "salt": "GWEcrcTP",
+                "md5": "c1c0cfe2fffc4fcaaa96d585c78abb2c",
+                "sha1": "c2062ec40fcbe9adf7b6da5685389013854b96a1",
+                "sha256": "d188341d10069fcb9cfe0dc39a7fb5024ed8abaa1b7da0a1d1fd6c3794ba9647"
+    },
+            "dob": {
+                "date": "1993-09-01T09:18:05Z",
+                "age": 25
+    },
+            "registered": {
+                "date": "2007-04-06T14:16:06Z",
+                "age": 12
+    },
+            "phone": "79777104",
+            "cell": "31285630",
+            "id": {
+                "name": "CPR",
+                "value": "728349-7472"
+    },
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/men/11.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/11.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/11.jpg"
+    },
+            "nat": "DK"
+    },
+        {
+            "gender": "male",
+            "name": {
+                "title": "mr",
+                "first": "owen",
+                "last": "lam"
+    },
+            "location": {
+                "street": "4839 3rd st",
+                "city": "shelbourne",
+                "state": "alberta",
+                "postcode": "A2O 8K0",
+                "coordinates": {
+                    "latitude": "4.4380",
+                    "longitude": "102.7928"
+    },
+                "timezone": {
+                    "offset": "+5:00",
+                    "description": "Ekaterinburg, Islamabad, Karachi, Tashkent"
+    }
+            },
+            "email": "owen.lam@example.com",
+            "login": {
+                "uuid": "7040bab3-24e4-41bf-bbb2-17534e9a61b3",
+                "username": "heavysnake833",
+                "password": "magicman",
+                "salt": "mZbQTZcV",
+                "md5": "c312826f5eb959041dbf7e3614c502fc",
+                "sha1": "569570b069485bac6162c1eb8d087a2be7630ac9",
+                "sha256": "418e2c7b1de7d11fdf6bcb096f3e40a4385b0e10c620266a22a6f5581eb537ed"
+    },
+            "dob": {
+                "date": "1948-01-28T08:03:23Z",
+                "age": 71
+    },
+            "registered": {
+                "date": "2005-07-07T21:01:47Z",
+                "age": 14
+    },
+            "phone": "671-018-2583",
+            "cell": "494-767-2240",
+            "id": {
+                "name": "",
+                "value": null
+    },
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/men/80.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/80.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/80.jpg"
+    },
+            "nat": "CA"
+    },
+        {
+            "gender": "female",
+            "name": {
+                "title": "mrs",
+                "first": "holly",
+                "last": "cooper"
+    },
+            "location": {
+                "street": "6350 coronation avenue",
+                "city": "greymouth",
+                "state": "taranaki",
+                "postcode": 92013,
+                "coordinates": {
+                    "latitude": "66.7994",
+                    "longitude": "-56.4363"
+    },
+                "timezone": {
+                    "offset": "-10:00",
+                    "description": "Hawaii"
+    }
+            },
+            "email": "holly.cooper@example.com",
+            "login": {
+                "uuid": "7fc6b422-c7a7-41b1-a3d0-abd0c97dd13c",
+                "username": "redfrog832",
+                "password": "vagabond",
+                "salt": "h6kePsck",
+                "md5": "75cedc025b46e87c4971bbba6293a0fc",
+                "sha1": "1dfc072bbf9929c418254cb0f6580c3d8ba58978",
+                "sha256": "542a30ed4ab8a2d85118d0800a881fb645a6aebefac7b55c40dd66a7c7b2b080"
+    },
+            "dob": {
+                "date": "1985-10-07T10:19:19Z",
+                "age": 33
+    },
+            "registered": {
+                "date": "2018-03-30T12:02:59Z",
+                "age": 1
+    },
+            "phone": "(360)-559-7444",
+            "cell": "(051)-360-9331",
+            "id": {
+                "name": "",
+                "value": null
+    },
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/women/57.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/women/57.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/women/57.jpg"
+    },
+            "nat": "NZ"
+    },
+        {
+            "gender": "male",
+            "name": {
+                "title": "mr",
+                "first": "kylian",
+                "last": "marchand"
+    },
+            "location": {
+                "street": "578 rue de l'abbé-carton",
+                "city": "lille",
+                "state": "nièvre",
+                "postcode": 40726,
+                "coordinates": {
+                    "latitude": "27.8762",
+                    "longitude": "81.9023"
+    },
+                "timezone": {
+                    "offset": "+7:00",
+                    "description": "Bangkok, Hanoi, Jakarta"
+    }
+            },
+            "email": "kylian.marchand@example.com",
+            "login": {
+                "uuid": "112e39d5-6f7c-4ab3-8700-38d60db6191a",
+                "username": "smallfish306",
+                "password": "4417",
+                "salt": "aSRYR4rO",
+                "md5": "5e4b4174db4bf40d14787da54eb3615b",
+                "sha1": "a2cf8184427a179ec1539ec97a050362598c4026",
+                "sha256": "a1dbd7d30a5667ce34e2738175489489d7b42ff7d3d82fe62d9ee0998c1db908"
+    },
+            "dob": {
+                "date": "1955-11-19T17:05:09Z",
+                "age": 63
+    },
+            "registered": {
+                "date": "2008-02-19T23:10:20Z",
+                "age": 11
+    },
+            "phone": "02-50-30-94-44",
+            "cell": "06-62-75-79-32",
+            "id": {
+                "name": "INSEE",
+                "value": "1NNaN10855457 41"
+    },
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/men/75.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+    },
+            "nat": "FR"
+    }
+    ],
+    "info": {
+        "seed": "ae428b42443ac02c",
+        "results": 5,
+        "page": 1,
+        "version": "1.2"
+    }
+}
+const list=data.results;
